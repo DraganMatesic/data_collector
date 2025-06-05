7ca835cb4d61f9f789bc3c77a265d5beeb75c3a0
DraganMatesic <dragan.matesic@gmail.com>
2025-06-05 19:49:16 +0200

    adding documentation and refactoring
    - adding documentation files in docs
    - refactoring database/core.py to database/main.py for clarity predicted further structure
    - removing sphinx from requirements
    - removing import sqlglot no longer used
    - comment formating

---
+++ data_collector/utilities/database/__init__.py
@@ -0,0 +1,7 @@
+"""
+Database Python package contains all logic that is needed for communication with database.
+It is separated to following modules:
+
+- main.py: contains major logic
+- loaders.py: helper module that checks if all prerequisites are met for using specific database
+"""
